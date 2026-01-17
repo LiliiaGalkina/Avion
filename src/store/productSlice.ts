e140d6c2d2faa28c063e8e21d394811bd71eaf31
Liliia Galkina <vvlili@yandex.ru>
2026-01-17 21:59:51 +0300

    начало redux slice

---
+++ src/store/productSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk, type PayloadAction } from "@reduxjs/toolkit";
+import type { IProduct, IProductState } from "./types";
+
+export const fetchProducts = createAsyncThunk<
+  IProduct[],
+  undefined,
+  { rejectValue: string }
+>("flights/fetchFlights", async function (_, { rejectWithValue }) {
+  const res = await fetch("./products.json");
+  if (!res.ok) {
+    return rejectWithValue("Server Error!");
+  }
+  const data = await res.json();
+  return data;
+});
+
+const initialState: IProductState = {
+	products: [],
+	filtered: [],
+	filterByCategory: [],
+	filterByPrice: [],
+	cart: [],
+	isLoading: false,
+	isError: false
+}
+
+const productSlice = createSlice({
+	name: "products",
+	initialState,
+	reducers: {
+		sortProductsPrice: (state) => {
+			state.products = state.filtered.sort((a,b) => a.price - b.price)
+		}
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchProducts.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchProducts.fulfilled,
+				(state, action: PayloadAction<IProduct[]>) => {
+					state.products = action.payload;
+					state.filtered = action.payload;
+					state.isLoading = false;
+					state.isError = false;
+				})
+			.addCase(fetchProducts.rejected, (state) => {
+				state.isLoading = false;
+				state.isError = true;
+		}) 
+	}
+})
+
+export const { sortProductsPrice } = productSlice.actions;
+
+export default productSlice.reducer;
---
+++ src/store/types.ts
@@ -0,0 +1,21 @@
+export interface IProduct {
+	id: string;
+	name: string;
+	price: number;
+	category: string;
+	image: string;
+	description: string;
+	height: number;
+	width: number;
+	depth: number
+}
+
+export interface IProductState {
+  products: IProduct[];
+  filtered: IProduct[];
+  filterByCategory: string[];
+  filterByPrice: number[];
+  cart: IProduct[];
+  isLoading: boolean;
+  isError: boolean;
+}
